27e1a4ac12485e60404312f8a561c5d72b7a8c35
Bruno Brustoloni e Oliveira <bruno.oliveira.external@ynap.com>
2023-08-02 19:22:44 +0100

    change (feature): model and mapper

--- features/src/main/kotlin/com/bbrustol/features/GenericScreen.kt
+++ features/src/main/kotlin/com/bbrustol/features/GenericScreen.kt
@@ -1,15 +1,13 @@
-package com.bbrustol.features.home
+package com.bbrustol.features
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.bbrustol.features.home.compose.CardHeadline
 import com.bbrustol.features.home.compose.ErrorScreen
-import com.bbrustol.features.home.model.HeadlineModel
 
 @Composable
 fun ShowGenericError(message: String, onRetryAction:() -> Unit) {
@@ -29,15 +27,5 @@ fun IsLoading() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         CircularProgressIndicator()
-    }
-}
-
-@Composable
-fun HeadlineScreen(headlineModel: List<HeadlineModel>) {
-    val state = rememberLazyListState()
-    LazyColumn(state = state) {
-        items(headlineModel.size) {
-            CardHeadline(headlineModel[it])
-        }
     }
 }
--- features/src/main/kotlin/com/bbrustol/features/home/model/HeadlineModel.kt
+++ features/src/main/kotlin/com/bbrustol/features/home/model/HeadlineModel.kt
@@ -3,7 +3,7 @@ package com.bbrustol.features.home.model
 data class HeadlineModel(
     val id: String,
     val author: String,
-    val content: String,
+    val content: String?,
     val description: String,
     val publishedAt: String,
     val sourceName: String,

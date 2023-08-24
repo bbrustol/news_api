5340fb47c56f7c8726c1b22499c7275b50aea08f
Bruno Brustoloni e Oliveira <bruno.oliveira.external@ynap.com>
2023-08-24 11:26:54 +0100

    feat (buildSrc): add domain object

--- buildSrc/src/main/kotlin/Modules.kt
+++ buildSrc/src/main/kotlin/Modules.kt
@@ -1,5 +1,7 @@
 object Modules {
     const val CORE = ":core"
-    const val UIKIT = ":uikit"
+    const val DOMAIN = ":domain"
     const val FEATURES = ":features"
+    const val KIT_SUPPORT = ":kit:support"
+    const val KIT_UI = ":kit:ui"
 }

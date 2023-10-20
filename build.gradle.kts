052242511163b2d59dcf91bc54d7c80ad9f79b0b
Bruno Brustoloni e Oliveira <bruno.oliveira@servexternos.santander.pt>
2023-10-20 14:32:11 +0100

    change (gradle): remove comment code from build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,3 @@
-//@Suppress("DSL_SCOPE_VIOLATION")
-//plugins {
-//    id("com.android.application") version "8.1.2" apply false
-//    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
-//    id("com.google.dagger.hilt.android") version "2.48.1" apply false
-////    id(libs.plugins.android.application.get().pluginId)
-////    id(libs.plugins.kotlin.android.get().pluginId)
-////    alias(libs.plugins.hilt) apply (false)
-//}
-
-
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     repositories {
